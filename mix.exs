18fa60b5afb8c65587ae49d273d068345fa4d409
Ryan Lee <drdgvhbh@gmail.com>
2020-06-06 12:16:07 -0400

    Add Travis Config

--- mix.exs
+++ mix.exs
@@ -6,8 +6,19 @@ defmodule GfsUmbrella.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ],
+      test_paths: test_paths(),
+      test_coverage: [tool: ExCoveralls]
     ]
+  end
+
+  defp test_paths do
+    "apps/*/test" |> Path.wildcard() |> Enum.sort()
   end
 
   # Dependencies listed here are available only for this
@@ -16,6 +27,8 @@ defmodule GfsUmbrella.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+      {:excoveralls, "~> 0.7", only: :test}
+    ]
   end
 end
